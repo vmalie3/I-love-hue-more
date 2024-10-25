5ffb6a639a7abbf650d2969d22bb31841608116d
Landen <brookslanden@gmail.com>
2024-10-25 16:25:44 -0400

    changed colors/sizes and added more levels

--- src/constants/sections/A/levels-list.js
+++ src/constants/sections/A/levels-list.js
@@ -40,10 +40,10 @@ const levelsListA = [
     title: "Level 4",
     id: 4,
     section: SECTIONS.A,
-    colors: ['#87dff5', '#f587ef', '#f5f387', '#f58792'],
+    colors: ['#66f261', '#6c36eb', '#d6c227', '#de18a3'],
     size: {
-      height: 10,
-      width: 10
+      height: 5,
+      width: 8
     },
     disabled: false
   },
@@ -54,7 +54,7 @@ const levelsListA = [
     colors: ['#d465fc', '#6590fc', '#a600e3', '#1304bf'],
     size: {
       height: 5,
-      width: 8
+      width: 9
     },
     disabled: false
   },
@@ -62,31 +62,56 @@ const levelsListA = [
     title: "Level 6",
     id: 6,
     section: SECTIONS.A,
-    disabled: true
+    colors: ['#a6edb4', '#56a856', '#32c732', '#038a1e'],
+    size: {
+      height: 6,
+      width: 10
+    },
+    disabled: false
   },
   {
     title: "Level 7",
     id: 7,
     section: SECTIONS.A,
-    disabled: true
+    colors: ['#e3e693', '#cbf5dd', '#f06e67', '#9c5f6a'],
+    size: {
+      height: 6,
+      width: 10
+    },
+    disabled: false
   },
   {
     title: "Level 8",
     id: 8,
     section: SECTIONS.A,
-    disabled: true
+    colors: ['#3fd183', '#6d3578', '#f5d625', '#e03895'],
+    size: {
+      height: 8,
+      width: 10
+    },
+    disabled: false
   },
   {
     title: "Level 9",
     id: 9,
     section: SECTIONS.A,
-    disabled: true
+    colors: ['#f77c94', '#bd4085', '#b51702', '#85192f'],
+    size: {
+      height: 9,
+      width: 9
+    },
+    disabled: false
   },
   {
     title: "Level 10",
     id: 10,
     section: SECTIONS.A,
-    disabled: true
+    colors: ['#87dff5', '#f587ef', '#f5f387', '#f58792'],
+    size: {
+      height: 10,
+      width: 10
+    },
+    disabled: false
   }
 ];
 
